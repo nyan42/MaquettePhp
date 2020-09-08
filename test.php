af472a224413b646bb5ecf597aaefc0a846ab3aa
nyan42 <victor-legros2@hotmail.fr>
2020-09-09 00:31:20 +0200

    transformation mysql -> PDO

--- test.php
+++ test.php
@@ -2,16 +2,64 @@
 function ShowProduct($BestSellers)
 {
 
-    $mysqli = new mysqli('localhost', 'root', '', 'e-boutique');
-    $mysqli->set_charset("utf8");
-    $requete = 'SELECT * FROM product';
-    $resultat = $mysqli->query($requete);
-    while ($product = $resultat->fetch_assoc()) {
+    // Database settings
+    $db = "e-boutique";
+    $dbhost = "localhost";
+    $dbport = 3306;
+    $dbuser = "root";
+    $dbpasswd = "";
+    try {
+        $pdo = new PDO('mysql:host=' . $dbhost . ';port=' . $dbport . ';dbname=' . $db . '', $dbuser, $dbpasswd);
+        $pdo->exec("SET CHARACTER SET utf8");
+        $reponse = $pdo->query('SELECT * FROM product');
+
+        while ($product = $reponse->fetch()) {
 ?>
-        <?php if ($BestSellers == "slider") { ?>
-            <div class="owl-item product_slider_item">
-                <div class="product-item">
-                    <div class="product discount">
+            <?php if ($BestSellers == "slider") { ?>
+                <div class="owl-item product_slider_item">
+                    <div class="product-item">
+                        <div class="product discount">
+                            <div class="product_image">
+                                <?php echo '<img src="images/' . $product['image'] . '" alt=' . $product['image'] . '>'; ?>
+                            </div>
+                            <?php if (is_null($product['reduction']) || $product['reduction'] == 0) {
+                                //plein tarif
+                                echo '<div class="favorite favorite_right"></div>'; ?>
+                                <div class="product_info">
+                                    <h6 class="product_name"><a> <?php echo $product['name']; ?> </a></h6>
+                                    <div class="product_price">
+                                        <?php
+                                        echo '$' . $product['price'];
+                                        ?>
+                                    </div>
+                                </div>
+                            <?php } else {
+                                //réduction
+                                echo '<div class="favorite favorite_left"></div>';
+                                echo '<div class="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center">
+            <span>-' . $product['reduction'] . '%</span></div>'; ?>
+
+                                <div class="product_info">
+                                    <h6 class="product_name"><a> <?php echo $product['name']; ?> </a></h6>
+                                    <div class="product_price">
+                                        <?php
+                                        echo '$' . ($product['price'] * (1 - $product['reduction'] / 100)) . '<span>$' . $product['price'] . '</span>';
+                                        ?>
+                                    </div>
+                                </div>
+
+                            <?php
+                            }
+                            ?>
+                        </div>
+                        <div class="red_button add_to_cart_button"><a href="#" onclick="">add to cart</a></div>
+
+                    </div>
+                <?php echo '</div>';
+            } else { ?>
+                    <?php echo '<div class="product-item ' . $product['genre'] . '">'; ?>
+
+                    <div class="product discount product_filter">
                         <div class="product_image">
                             <?php echo '<img src="images/' . $product['image'] . '" alt=' . $product['image'] . '>'; ?>
                         </div>
@@ -19,7 +67,7 @@ function ShowProduct($BestSellers)
                             //plein tarif
                             echo '<div class="favorite favorite_right"></div>'; ?>
                             <div class="product_info">
-                                <h6 class="product_name"><a  > <?php echo $product['name']; ?> </a></h6>
+                                <h6 class="product_name"><a> <?php echo $product['name']; ?> </a></h6>
                                 <div class="product_price">
                                     <?php
                                     echo '$' . $product['price'];
@@ -30,10 +78,10 @@ function ShowProduct($BestSellers)
                             //réduction
                             echo '<div class="favorite favorite_left"></div>';
                             echo '<div class="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center">
-            <span>-' . $product['reduction'] . '%</span></div>'; ?>
+                        <span>-' . $product['reduction'] . '%</span></div>'; ?>
 
                             <div class="product_info">
-                                <h6 class="product_name"><a  > <?php echo $product['name']; ?> </a></h6>
+                                <h6 class="product_name"><a> <?php echo $product['name']; ?> </a></h6>
                                 <div class="product_price">
                                     <?php
                                     echo '$' . ($product['price'] * (1 - $product['reduction'] / 100)) . '<span>$' . $product['price'] . '</span>';
@@ -48,50 +96,9 @@ function ShowProduct($BestSellers)
                     <div class="red_button add_to_cart_button"><a href="#" onclick="">add to cart</a></div>
 
                 </div>
-            <?php echo '</div>';
-        } else { ?>
-                <?php echo '<div class="product-item ' . $product['genre'] . '">'; ?>
-
-                <div class="product discount product_filter">
-                    <div class="product_image">
-                        <?php echo '<img src="images/' . $product['image'] . '" alt=' . $product['image'] . '>'; ?>
-                    </div>
-                    <?php if (is_null($product['reduction']) || $product['reduction'] == 0) {
-                        //plein tarif
-                        echo '<div class="favorite favorite_right"></div>'; ?>
-                        <div class="product_info">
-                            <h6 class="product_name"><a  > <?php echo $product['name']; ?> </a></h6>
-                            <div class="product_price">
-                                <?php
-                                echo '$' . $product['price'];
-                                ?>
-                            </div>
-                        </div>
-                    <?php } else {
-                        //réduction
-                        echo '<div class="favorite favorite_left"></div>';
-                        echo '<div class="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center">
-                        <span>-' . $product['reduction'] . '%</span></div>'; ?>
-
-                        <div class="product_info">
-                            <h6 class="product_name"><a  > <?php echo $product['name']; ?> </a></h6>
-                            <div class="product_price">
-                                <?php
-                                echo '$' . ($product['price'] * (1 - $product['reduction'] / 100)) . '<span>$' . $product['price'] . '</span>';
-                                ?>
-                            </div>
-                        </div>
-
-                    <?php
-                    }
-                    ?>
-                </div>
-                <div class="red_button add_to_cart_button"><a href="#" onclick="">add to cart</a></div>
-
-            </div>
-        <?php
-        }
-        ?>
+            <?php
+            }
+            ?>
 
 
 
@@ -99,7 +106,10 @@ function ShowProduct($BestSellers)
 
 
 <?php
+        }
+        $reponse->closeCursor();
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
     }
-    $mysqli->close();
 }
 ?>
